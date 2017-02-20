0ee7feb16168c0d87ca80d655b9314dde1aedaf6
nikhiljmindfire <nikhilj@mindfiresolutions.com>
2017-02-20 13:49:57 +0530

    Create index.ts

---
+++ src/app/Modules/AccountModule/index.ts
@@ -0,0 +1,4 @@
+export { LoginComponent } from './LoginComponent/';
+export { RegistrationComponent } from './RegistrationComponent/';
+export { ForgetPasswordComponent } from './ForgetPasswordComponent/';
+export * from './AccountModule';
