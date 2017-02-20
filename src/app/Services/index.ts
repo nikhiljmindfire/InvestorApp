bbd3c592c862b8683ee877ac884a3a57a4b306c6
nikhiljmindfire <nikhilj@mindfiresolutions.com>
2017-02-20 13:36:17 +0530

    Create index.ts

---
+++ src/app/Services/index.ts
@@ -0,0 +1,4 @@
+export { AuthService } from './AuthorizationServices/AuthorizationServices';
+export { DialogServices } from './DialogServices/DialogServices';
+export { ToastServices } from './ToastServices/ToastServices';
+export { UserRegisterationServices } from './UserRegisterationServices/UserRegisterationServices';
