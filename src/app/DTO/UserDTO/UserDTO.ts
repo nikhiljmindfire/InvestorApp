18026e2355b745f9bbc7f65ff67f47222da091c2
nikhiljmindfire <nikhilj@mindfiresolutions.com>
2017-02-20 13:26:30 +0530

    Create UserDTO.ts

---
+++ src/app/DTO/UserDTO/UserDTO.ts
@@ -0,0 +1,9 @@
+export interface IUsers {
+    Id?: number,
+    Name: string,
+    UserName: string,
+    Email: string,
+    Gender?: boolean,
+    Password: string,
+    ConfirmPassword: string
+}
